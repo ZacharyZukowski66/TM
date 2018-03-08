62bcfc4225f197f6c670ae78ae946a7518050d43
Zack Zukowski <ZackZukowski66@gmail.com>
2018-03-07 21:09:48 -0800

    was dumb and forgot to add this

--- TM.java
+++ TM.java
@@ -1,225 +1,190 @@
 import java.util.*;
 import java.io.*;
 import java.time.*;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
 
 public class TM{
-   public void appMain(String[]args){
-      TaskLog log = new TaskLog("TM.log");
-      ArrayList<Task> tasks = new ArrayList<Task>();
-      String command;
-      LocalDateTime t;
-      try{
-         switch(args[0]){
-            case "start": t = LocalDateTime.now();
-                          if(args.length == 2){
-                               log.writeLine((t + "\t" + args[1] + "\tstart"), "TM.log");
-                          }
-                          else{
-                              this.usage();
-                          }
-                          break;
-            case "stop": t = LocalDateTime.now();
-                         if(args.length == 2){
-                              log.writeLine((t + "\t" + args[1] + "\tstop"),"TM.log");
-                         }
-                         else{
-                             this.usage();
-                         }
-                         break;
-             case "describe": t = LocalDateTime.now();
-                              if(args.length == 3){
-                                 log.writeLine((t + "\t" + args[1] + "\tdescribe\t" + args[2]),"TM.log");
-                              }
-                              else if(args.length == 4){
-                                 log.writeLine((t + "\t" + args[1] + "\tdescribe\t" + args[2] + "\t" + args[3]),"TM.log");
-                              }
-                              else{
-                                 this.usage();
-                              }
-                              break;
-             case "size": t = LocalDateTime.now();
-                          if(args.length == 3){
-                              log.writeLine((t + "\t" + args[1] + "\tsize\t" + args[2]),"TM.log");
-                          }
-                          else{
-                              this.usage();
-                          }
-                          break;
-             case "summary": t = LocalDateTime.now();
-                             tasks = log.read("TM.log", tasks);
-                             System.out.println("\t     summary\n\t_________________");
-                             if(args.length == 2){
-                                 for(Task tsk: tasks){
-                                    if((tsk.name).equals(args[1])){
-                                       System.out.println(tsk);
-                                       break;
-                                    }
-                                 }
-                             }
-                             else if(args.length == 1){
-                                 for(Task tsk: tasks){
-                                    System.out.println(tsk);
-                                 }
-                             }
-                             else{
-                                  this.usage();
-                             }
-                             break;
-            default: this.usage();
-         }
-      }
-      catch(Exception e){
-         this.usage();
-      }
-   }
-//   public boolean logCommand(String... cmd){
-//      
-//   }
-//////////////////////////////////////////////////////////////////////////////
+   TMModel tmModel = new TMModel();
+   LocalDateTime timeStamp = LocalDateTime.now();
+   String log = "TM.txt";
+///////////////////////////////////////////////////////////////////////////////
    public static void main(String[]args){
       TM tm = new TM();
       tm.appMain(args);
    }
-   public void usage(){
-      System.out.println("Please use the following format for commands:\n\n" +
-                         "java TM start <task name>\n" +
-                         "java TM stop <task name>\n" +
-                         "java TM describe <task name> <description in quotation marks>\n" +
-                         "java TM describe <task name> <description in quotation marks> <size(Ex:S, M, L, XL)>\n" +
-                         "java TM size <task name> <size(Ex:S, M, L, XL)>\n" +
-                         "java TM rename <old task name> <new task name that isn't in TM.log>\n" +
-                         "java TM delete <task name>\n" +
-                         "java TM summary <task name>\n" +
-                         "java TM summary\n" +
-                         "Log is named TM.log");
-   }
-//////////////////////////////////////////////////////////////////////////////
-   private class TaskLog{
-      TaskLog(String name){
-         File f = new File(name);
-      }
-      public void writeLine(String s, String n){
-         try{
-            BufferedWriter writer = new BufferedWriter(new FileWriter(n, true));
-            writer.append(s);
-            writer.newLine();
-            writer.close();
-         }catch(IOException e){
-            e.printStackTrace(); 
+///////////////////////////////////////////////////////////////////////////////
+   public void appMain(String[]args){
+      int numArgs = args.length;
+      String cmd = "";
+      String nm = "";
+      String des = "";
+      String sz = "";
+      String newNm = "";
+      if(numArgs > 0 && numArgs < 5){
+         cmd = args[0];
+         if(numArgs > 1){
+            nm = args[1];
+         }
+         switch(cmd){
+            case("Start"): if(numArgs < 2 || numArgs > 2){                             
+                              usage();
+                              break;
+                           }
+                           tmModel.startTask(nm);
+                           break;
+            case("Stop"):  if(numArgs < 2 || numArgs > 2){
+                              usage();
+                              break;
+                           }
+                           tmModel.stopTask(nm);
+                           break;
+            case("Describe"): if(numArgs < 3){
+                                 usage();
+                                 break;
+                              }
+                              des = args[2];
+                              tmModel.describeTask(nm, des);
+                              if(numArgs > 3){
+                                 sz = args[3];
+                                 tmModel.sizeTask(nm, sz);
+                              }
+                              break;
+            case("Size"):  if(numArgs < 3 || numArgs > 4){
+                              usage();
+                              break;
+                           }
+                           sz = args[2];
+                           tmModel.sizeTask(nm, sz);
+                           break;
+            case("Delete"): if(numArgs < 2 || numArgs > 2){
+                                 usage();
+                                 break;
+                            }
+                            tmModel.deleteTask(nm);
+                            break;
+            case("Rename"): if(numArgs < 3 || numArgs > 3){
+                                 usage();
+                                 break;
+                            } 
+                            newNm = args[2];
+                            tmModel.renameTask(nm, newNm);
+                            break;
+            case("Summary"): try{
+                                 cmdSummary(nm);
+                             }
+                             catch(IOException e){
+                                 usage();
+                             }
+                             break;
+            default: usage();
+                     break;
          }
       }
-      public ArrayList<Task> read(String n, ArrayList<Task> ta){
-          ArrayList<Task> t = ta;
-          String nm = null;
-          String cmd = null;
-          String data1 = null;
-          String data2 = null;
-          LocalDateTime tm = null;
-          boolean changed = false;
-          try{
-             BufferedReader br = new BufferedReader(new FileReader(n));
-             String strLine;
-             while((strLine = br.readLine()) != null){
-                StringTokenizer st = new StringTokenizer(strLine, "\t");
-                DateTimeFormatter formatter = DateTimeFormatter.ISO_DATE_TIME;
-                if(st.hasMoreTokens()){
-                   tm = LocalDateTime.parse((st.nextToken()), formatter);
-                }
-                if(st.hasMoreTokens()){
-                   nm = st.nextToken();
-                }
-                if(st.hasMoreTokens()){    
-                   cmd = st.nextToken(); 
-                }
-                if(st.hasMoreTokens()){                 
-                   data1 = st.nextToken();
-                }
-                if(st.hasMoreTokens()){
-                   data2 = st.nextToken();
-                }
-                data2 = "";
-                changed = false;
-                for(int i = 0; i < t.size(); i++){
-                   if(((t.get(i)).name).equals(nm)){
-                      if(cmd.equals("Start")){
-                           ((t.get(i)).latestStart) = tm;
-                      }
-                      else if(cmd.equals("Stop")){
-                           ((t.get(i)).stop) = tm;
-                           if((t.get(i)).latestStart != null){
-                              ((t.get(i)).total) += ChronoUnit.SECONDS.between(((t.get(i)).latestStart),((t.get(i)).stop));
-                              ((t.get(i)).latestStart) = null;
-                              ((t.get(i)).stop) = null;
-                           }
-                      }
-                      else if(cmd.equals("Describe")){
-                           if(data2.equals(null)){
-                              ((t.get(i)).description) += ("\n\t     " + data1);
-                           }
-                           else{
-                              ((t.get(i)).description) += ("\n\t     " + data1);
-                              ((t.get(i)).size) = data2;
-                           }
-                      }
-                      else if(cmd.equals("Size")){
-                           ((t.get(i)).size) = data1;
-                      }
-                      changed = true;
-                   }
-                }
-                if(changed == false || t.isEmpty()){
-                     if(cmd.equals("Start")){
-                        t.add(new Task(nm, " ", " ", tm, null));
-                     }
-                     else if(cmd.equals("Stop")){
-                        t.add(new Task(nm, " ", " ", null, null));
-                     }
-                     else if(cmd.equals("Describe")){
-                        if(data2.equals(null)){
-                           t.add(new Task(nm, data1, " ", null, null));
-                        }
-                        else{
-                           t.add(new Task(nm, data1, data2, null, null));
-                        }
-                     }
-                     else if(cmd.equals("Size")){
-                           t.add(new Task(nm, " ", data1, null, null));
-                     }
-                }      
-             }
-             br.close();
-          }catch(IOException e){
-             e.printStackTrace();
-          }       
-          return t;
+      else{
+         usage();
       }
    }
-//////////////////////////////////////////////////////////////////////////////
-   private class Task{
-      String name;
-      String description;
-      String size;
-      long total;
-      LocalDateTime latestStart;
-      LocalDateTime stop;
-      Task(String n, String d, String s, LocalDateTime lstar, LocalDateTime sto){
-         name = n;
-         description = d;
-         size = s;
-         latestStart = lstar;
-         stop = sto;
+///////////////////////////////////////////////////////////////////////////////
+   public void cmdSummary(String nm) throws IOException{
+      Task current;
+      try{
+         if(nm.equals("")){
+            sumAll();
+         }
+         else if(taskExists(nm) == true){
+            current = new Task(nm);
+            current.sum();
+         }
       }
-      public String getTotal(long t){
-         long hours = t/3600;
-         long minutes = (t%3600)/60;
-         long seconds = (t%60);
-         return (String.format("%02d:%02d:%02d", hours, minutes, seconds));
-      }
-      public String toString(){
-         return ("Name: " + name + "\nSize: " + size + "\nDescription: " + 
-                  description + "\nTotal Time: " + this.getTotal(total) + "\n");
+      catch(IOException e){
+         usage();
       }
    }
-}
+///////////////////////////////////////////////////////////////////////////////
+   public String getNm(String line){
+      StringTokenizer stk = new StringTokenizer(line, "\t");
+      return stk.nextToken();
+   }
+///////////////////////////////////////////////////////////////////////////////
+   public String getCmd(String line){
+      StringTokenizer stk = new StringTokenizer(line, "\t");
+      stk.nextToken(); //skips nm
+      return stk.nextToken();
+   }
+///////////////////////////////////////////////////////////////////////////////
+   public String getDat(String line){
+      StringTokenizer stk = new StringTokenizer(line, "\t");
+      stk.nextToken(); //skips nm
+      stk.nextToken(); //skips cmd
+      return stk.nextToken();
+   }
+///////////////////////////////////////////////////////////////////////////////
+   public boolean taskExists(String nm) throws IOException{
+      Set<String> names = tmModel.taskNames();
+      if(names.contains(nm)){
+         return true;
+      }
+      return false;
+   }
+///////////////////////////////////////////////////////////////////////////////
+   public void sumAll() throws IOException{
+      Set<String> names = tmModel.taskNames();
+      Set<Task> tasks = new HashSet<>();
+      if(names.size() > 0){
+         System.out.println("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+         names.forEach(i -> {
+            try{
+               tasks.add(new Task(i));
+            }
+            catch(IOException e){
+               usage();
+            }
+         });
+         tasks.forEach(i -> {
+            i.sum();
+         });
+         sumSize();
+         System.out.println("Total Time Spent on all Task: " + tmModel.elapsedTimeForAllTasks());
+         System.out.println("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+      }
+   }
+///////////////////////////////////////////////////////////////////////////////
+   public void sumSize() throws IOException{
+      Set<String> names = new HashSet<>();
+      Set<String> sizes = tmModel.taskSizes();
+      sizes.forEach(i -> {
+         tmModel.taskNamesForSize(i).forEach(j -> {
+            names.add(j);
+         });
+         if(names.size() > 1){
+            System.out.println("Stats on " + i + " sized tasks:");
+            System.out.println("Min: " + tmModel.minTimeForSize(i));
+            System.out.println("Max: " + tmModel.maxTimeForSize(i));
+            System.out.println("Avg: " + tmModel.avgTimeForSize(i));
+         }
+         names.clear();
+      });
+   }
+///////////////////////////////////////////////////////////////////////////////
+    public void usage(){
+      System.out.println("Please use the following format for commands:\n\n" +
+                         "java TM Start <task name>\n" +
+                         "java TM Stop <task name>\n" +
+                         "java TM Describe <task name> <description in quotation marks>\n" +
+                         "java TM Describe <task name> <description in quotation marks> <size(Ex:S, M, L, XL)>\n" +
+                         "java TM Size <task name> <size(Ex:S, M, L, XL)>\n" +
+                         "java TM Rename <old task name> <new task name that isn't in TM.txt>\n" +
+                         "java TM Delete <task name>\n" +
+                         "java TM Summary <task name>\n" +
+                         "java TM Summary\n" +
+                         "Log is named TM.txt");
+   } 
+///////////////////////////////////////////////////////////////////////////////
+   public String timeStampFormat(long timeStamp){
+      long hours, mins;
+      hours = timeStamp/3600; //gets hours
+      timeStamp %= 3600;      //leaves only minutes and seconds
+      mins = timeStamp/60;    //gets minutes
+      timeStamp %= 60;        //leaves only seconds
+      return (hours + " hours, " + mins + " minutes, and " + timeStamp + " seconds.");
+   }
+} 
